bfd6883757669f59686fafb8b820a901e3d92c0a
Nazar Novak <nazar.novac_c@sandvik.com>
2023-02-17 10:51:02 +0100

    Fix cors?

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module friendy-be
 
 go 1.16
+
+require (
+	github.com/rs/cors v1.8.3
+	github.com/zenazn/goji v1.0.1
+)
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
+	"github.com/zenazn/goji/web"
 )
 
 type Incoming struct {
@@ -12,19 +15,61 @@ type Incoming struct {
 }
 
 
-func test(rw http.ResponseWriter, req *http.Request) {
-	decoder := json.NewDecoder(req.Body)
+func test() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
 
-	var in Incoming
-	err := decoder.Decode(&in)
-	if err != nil {
-		log.Fatalln(err)
+		var in Incoming
+		err := decoder.Decode(&in)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println(in.Msg)
 	}
-
-	fmt.Println(in.Msg)
 }
   
 func main() {
-    http.HandleFunc("/test", test)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router(),
+	}
+
+	fmt.Println("Running on port :8080")
+
+	// Start the server and log any errors it returns
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(fmt.Sprintf("error running server: %s", err.Error()))
+	}
+}
+
+
+func router() *web.Mux {
+	mux := web.New()
+
+	mux.Use(getCorsHandler())
+
+	mux.Post("/api/test", test())
+
+	//mux.Get("/api/test", Test())
+
+	return mux
+}
+
+func getCorsHandler() func(http.Handler) http.Handler {
+	allowedOrigins := []string{}
+// TODO: Add mode dev + mode prod here to separate sites
+	allowedOrigins = append(allowedOrigins, "http://localhost:3000")
+	// External IP
+	// allowedOrigins = append(allowedOrigins, "http://84.219.232.19:3000")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   allowedOrigins,
+		AllowedHeaders:   []string{"Accept", "Authorization", "Cache-Control", "Content-Type", "Origin", "User-Agent", "Viewport", "X-Requested-With"},
+		MaxAge:           1728000,
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST"},
+	})
+
+	return c.Handler
 }
